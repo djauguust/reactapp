4419a44e092fa5c4fc827e0ef40a7d0cfe2a44e0
Augustot <dj.august.brito@gmail.com>
2021-07-18 22:29:12 -0300

    quitar contraseña

--- src/App.js
+++ src/App.js
@@ -23,11 +23,7 @@ function App() {
           <label htmlFor="apellido">Apellido</label>
           <input type="text" name="apellido" id="apellido" onChange={ event => setApellido(event.target.value) }/>
       </div>
-      <div>
-          
-            <label htmlFor="contraseña">Contraseña</label>
-            <input type="text" name="contraseña" id="contraseña" />
-        </div>
+      
 
 <button onClick={mostrarInformacion}> Mostrar Información </button>
 
